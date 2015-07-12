ed60cd6c81dbe6db9aa9f21db83ef3ef94a3d892
Spirit_xc <xc1129@gmail.com>
2015-07-13 01:25:28 +0800

    封装数据库操作函数

---
+++ admin/doLogin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Spirit_xc
+ * Date: 15/7/13
+ * Time: 上午12:56
+ */
+
+$username=$_POST['username'];
+$password=$_POST['password'];
+$verify=$_POST['verify'];
+$verify1=$_SESSION['verify'];
+
---
+++ configs/configs.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Spirit_xc
+ * Date: 15/7/13
+ * Time: 上午1:00
+ */
+
+define("DB_HOST","localhost");
+define("DB_USER","test");
+define("DB_PWD","123456");
+define("DB_NAME","myshop");
+define("DB_CHARSET","utf8");
